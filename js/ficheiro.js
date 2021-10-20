0888a25eaa2517194dd5baeb7f2262cb2b66a07a
Oscar Alberto Guiamba <oscarguiamba.a@gmail.com>
2021-10-20 07:56:18 -0700

    melhorias

--- js/ficheiro.js
+++ js/ficheiro.js
@@ -1,6 +1,9 @@
 var meuIcon = document.querySelector(".menu_icon");
 var sideBar = document.querySelector(".sideBar");
+var container = document.querySelector(".container");
 
 meuIcon.onclick = function(){
     sideBar.classList.toggle("reduzSideBar");
+    container.classList.toggle("containerLargo");
 }
+
